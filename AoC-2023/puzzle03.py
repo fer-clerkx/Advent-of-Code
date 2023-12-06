70015c40f418a5fe296b9d0d7b30cec231b16487
Fer <ferclerkx@hotmail.com>
2023-12-06 18:32:18 +0100

    Finished day 3 part B

--- AoC-2023/puzzle03.py
+++ AoC-2023/puzzle03.py
@@ -55,30 +55,72 @@ def solvePuzzle03A():
 
 def solvePuzzle03B():
 	inputFile = open(FILE_PATH, "r", encoding="UTF-8")
-	numbers = []*3
-	gears = []*3
+	numbers = [[]]*3
+	gears = [[]]*3
 	sum = 0
 	while True:
 		line = inputFile.readline()
 		if line == "":
+			# Iterate through gears[0]
+			for gear in gears[0]:
+				adjNum = []
+				# Check numbers[1]
+				for number in numbers[1]:
+					if gear.start()+1 == number.start() or gear.end()-1 == number.end():
+						adjNum.append(number)
+				# Check numbers[0]
+				for number in numbers[0]:
+					if gear.start()+1 >= number.start() and gear.end()-1 <= number.end():
+						adjNum.append(number)
+				# If two numbers found -> calc product, add sum
+				if len(adjNum) == 2:
+					sum += int(adjNum[0].group('number')) * int(adjNum[1].group('number'))
 			break
 		paterns = (
 			"(?P<number>\d+)",
-			"(?P<gear>*)"
+			"(?P<gear>[*])"
 		)
 		regex = '|'.join(paterns)
 		matches = re.finditer(regex, line)
 
-		# Load matches into numbers and gears
-		# First 2 lines not finished?
-			# Skip to start of loop
-
 		# Shift numbers and gears
+		numbers[2] = numbers[1]
+		numbers[1] = numbers[0]
+		gears[2] = gears[1]
+		gears[1] = gears[0]
+
+		# Load matches into numbers and gears
+		numbers[0] = []
+		gears[0] = []
+		for match in matches:
+			if match.group('number'):
+				numbers[0].append(match)
+			elif match.group('gear'):
+				gears[0].append(match)
+
+		# First 2 lines not finished?
+		if numbers[2] == [] and gears[2] == []:
+			# Skip to start of loop
+			continue
+
 		# Iterate through gears[1]
+		for gear in gears[1]:
+			adjNum = []
 			# Check numbers[2]
+			for number in numbers[2]:
+				if gear.start()+1 >= number.start() and gear.end()-1 <= number.end():
+					adjNum.append(number)
 			# Check numbers[1]
+			for number in numbers[1]:
+				if gear.start()+1 == number.start() or gear.end()-1 == number.end():
+					adjNum.append(number)
 			# Check numbers[0]
+			for number in numbers[0]:
+				if gear.start()+1 >= number.start() and gear.end()-1 <= number.end():
+					adjNum.append(number)
 			# If two numbers found -> calc product, add sum
+			if len(adjNum) == 2:
+				sum += int(adjNum[0].group('number')) * int(adjNum[1].group('number'))
 	return sum
 
 

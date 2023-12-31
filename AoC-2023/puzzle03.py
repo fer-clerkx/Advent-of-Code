abba8eec97c4efdedd4e0b5c43c81c715ffd1205
Fer Clerkx <ferclerkx@hotmail.com>
2023-12-31 01:34:40 +0100

    Redid day 3.

--- AoC-2023/puzzle03.py
+++ AoC-2023/puzzle03.py
@@ -1,127 +1,32 @@
 import re
+import math
 
 FILE_PATH = f"input/input{__file__[-5:-3]}.txt"
 
-def main():
-    solutionA = solvePuzzle03A()
-    print("Solution A:", solutionA)
-    solutionB = solvePuzzle03B()
-    print("Solution B:", solutionB)
+with open(FILE_PATH) as input_file:
+    input_lines = input_file.read().split()
 
-def solvePuzzle03A():
-    inputFile = open(FILE_PATH, "r", encoding="UTF-8")
-    lastNumbers = []
-    lastSymbols = []
-    sum = 0
-    while True:
-        line = inputFile.readline()
-        if line == "":
-            break
-        paterns = (
-            "(?P<number>\d+)",
-            "(?P<symbol>[^\d.\s])"
-        )
-        regex = '|'.join(paterns)
-        matches = re.finditer(regex, line)
-        currentNumbers = []
-        currentSymbols = []
-        for match in matches:
-            if match.group('number'):
-                currentNumbers.append(match)
-            elif match.group('symbol'):
-                currentSymbols.append(match)
-        # Check previous row numbers
-        for number in lastNumbers:
-            for symbol in currentSymbols:
-                if symbol.end()-1 <= number.end() and symbol.start() >= number.start()-1:
-                    sum += int(number.group())
-                    break
-        lastNumbers = []
-        # Check previous row symbols
-        for symbol in lastSymbols:
-            for number in currentNumbers:
-                if symbol.end()-1 <= number.end() and symbol.start() >= number.start()-1:
-                    sum += int(number.group())
-        lastSymbols = currentSymbols
-        # Check horizontally and update lastNumbers
-        for number in currentNumbers:
-            lastNumbers.append(number)
-            for symbol in currentSymbols:
-                if symbol.end() == number.start() or symbol.start() == number.end():
-                    sum += int(number.group())
-                    lastNumbers.pop() # Don't check number again
-                    break
-    return sum
+symbols_by_pos = {(row, col): (symbol, [])
+                  for row, line in enumerate(input_lines)
+                  for col, symbol in enumerate(line)
+                  if symbol != "." and not symbol.isdigit()}
 
-def solvePuzzle03B():
-    inputFile = open(FILE_PATH, "r", encoding="UTF-8")
-    numbers = [[]]*3
-    gears = [[]]*3
-    sum = 0
-    while True:
-        line = inputFile.readline()
-        if line == "":
-            # Iterate through gears[0]
-            for gear in gears[0]:
-                adjNum = []
-                # Check numbers[1]
-                for number in numbers[1]:
-                    if gear.start()+1 == number.start() or gear.end()-1 == number.end():
-                        adjNum.append(number)
-                # Check numbers[0]
-                for number in numbers[0]:
-                    if gear.start()+1 >= number.start() and gear.end()-1 <= number.end():
-                        adjNum.append(number)
-                # If two numbers found -> calc product, add sum
-                if len(adjNum) == 2:
-                    sum += int(adjNum[0].group('number')) * int(adjNum[1].group('number'))
-            break
-        paterns = (
-            "(?P<number>\d+)",
-            "(?P<gear>[*])"
-        )
-        regex = '|'.join(paterns)
-        matches = re.finditer(regex, line)
+sum_parts = 0
+for row, line in enumerate(input_lines):
+    for match in re.finditer("\d+", line):
+        for d_row in (-1, 0, 1):
+            for col in range(match.start()-1, match.end()+1):
+                if (row+d_row, col) in symbols_by_pos:
+                    number = int(match.group(0))
+                    sum_parts += number
+                    symbols_by_pos[row+d_row, col][1].append(number)
 
-        # Shift numbers and gears
-        numbers[2] = numbers[1]
-        numbers[1] = numbers[0]
-        gears[2] = gears[1]
-        gears[1] = gears[0]
+sum_gears = sum(
+                math.prod(numbers)
+                for (symbol, numbers)in symbols_by_pos.values()
+                if symbol == "*" and len(numbers) > 1
+            )
 
-        # Load matches into numbers and gears
-        numbers[0] = []
-        gears[0] = []
-        for match in matches:
-            if match.group('number'):
-                numbers[0].append(match)
-            elif match.group('gear'):
-                gears[0].append(match)
+print("Solution 1:", sum_parts)
 
-        # First 2 lines not finished?
-        if numbers[2] == [] and gears[2] == []:
-            # Skip to start of loop
-            continue
-
-        # Iterate through gears[1]
-        for gear in gears[1]:
-            adjNum = []
-            # Check numbers[2]
-            for number in numbers[2]:
-                if gear.start()+1 >= number.start() and gear.end()-1 <= number.end():
-                    adjNum.append(number)
-            # Check numbers[1]
-            for number in numbers[1]:
-                if gear.start()+1 == number.start() or gear.end()-1 == number.end():
-                    adjNum.append(number)
-            # Check numbers[0]
-            for number in numbers[0]:
-                if gear.start()+1 >= number.start() and gear.end()-1 <= number.end():
-                    adjNum.append(number)
-            # If two numbers found -> calc product, add sum
-            if len(adjNum) == 2:
-                sum += int(adjNum[0].group('number')) * int(adjNum[1].group('number'))
-    return sum
-
-
-main()
+print("Solution 2:", sum_gears)

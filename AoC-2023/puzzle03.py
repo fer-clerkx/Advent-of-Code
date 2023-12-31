4d7815a902e0a1dac853a8c8f96b97d75602fffb
Fer Clerkx <ferclerkx@hotmail.com>
2023-12-31 16:26:28 +0100

    Slightly revised day 3.

--- AoC-2023/puzzle03.py
+++ AoC-2023/puzzle03.py
@@ -11,19 +11,19 @@ symbols_by_pos = {(row, col): (symbol, [])
                   for col, symbol in enumerate(line)
                   if symbol != "." and not symbol.isdigit()}
 
-sum_parts = 0
 for row, line in enumerate(input_lines):
     for match in re.finditer("\d+", line):
-        for d_row in (-1, 0, 1):
-            for col in range(match.start()-1, match.end()+1):
-                if (row+d_row, col) in symbols_by_pos:
-                    number = int(match.group(0))
-                    sum_parts += number
-                    symbols_by_pos[row+d_row, col][1].append(number)
+        boundary = {(row, col) for row in range(row-1, row+2)
+                               for col in range(match.start()-1,
+                                                match.end()+1)}
+        for symbol_pos in boundary & symbols_by_pos.keys():
+            symbols_by_pos[symbol_pos][1].append(int(match.group(0)))
+
+sum_parts = sum(sum(numbers) for (_, numbers) in symbols_by_pos.values())
 
 sum_gears = sum(
                 math.prod(numbers)
-                for (symbol, numbers)in symbols_by_pos.values()
+                for (symbol, numbers) in symbols_by_pos.values()
                 if symbol == "*" and len(numbers) > 1
             )
 
